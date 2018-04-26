3dc0d4a8dad2a34c5e5e2eacb5421432481f9404
Stephen Knupfer <stephenrknupfer@hotmail.com>
2018-04-25 22:37:22 -0300

    Adicionado tela de lista de barbearias

---
+++ UNASP.MBarber.UI.Web/Controllers/BarberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UNASP.MBarber.UI.Web.Controllers
+{
+    public class BarberController : Controller
+    {
+        // GET: Barber
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
